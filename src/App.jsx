2e60b1806ed9a2b8f8b356e8ab752f7aaf294396
mkulemeka <kahlekulemeka@gmail.com>
2023-09-23 16:57:00 -0400

    card is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css";
 import "./index.css";
 
+import Card from "./components/MuseumCard";
 import Nav from "./components/Nav";
+import data from "./museums.json";
 
 function App() {
+  console.log(data.map((museum) => museum.name));
+
   return (
     <section>
       <Nav />
+      <section className="flex justify-center flex-wrap gap-4">
+        {data.map((museum, index) => (
+          <Card key={index} museum={museum} />
+        ))}
+      </section>
     </section>
   );
 }
---
+++ src/components/MuseumCard.jsx
@@ -0,0 +1,39 @@
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Link,
+} from "@nextui-org/react";
+
+import PropTypes from "prop-types";
+
+const MuseumCard = ({ museum }) => {
+  const { name, address, type, price, link } = museum;
+  return (
+    <Card className="max-w-[400px]">
+      <CardHeader>
+        <h3 className="font-bold">{name}</h3>
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <address>{address}</address>
+        <h5>{type}</h5>
+        <p>{price}</p>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <Link isExternal showAnchorIcon href={link}>
+          {name}
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+};
+
+MuseumCard.propTypes = {
+  museum: PropTypes.object.isRequired,
+};
+
+export default MuseumCard;

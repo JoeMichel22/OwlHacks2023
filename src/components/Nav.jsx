9855070bbad6aed12dd473234fdba4f07a1bf34b
mkulemeka <kahlekulemeka@gmail.com>
2023-09-23 14:30:21 -0400

    more setup

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+const Nav = () => {
+  return (
+    <header className=" w-full">
+      <nav className="w-full flex justify-center font-bold">
+        <span>Museums in Philadelphia</span>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;

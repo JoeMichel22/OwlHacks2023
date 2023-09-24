fe875eb580637c9987d7fb5d84ed78550980ae72
Joe Michel <tun69277@temple.edu>
2023-09-24 08:55:37 -0400

    Added footer email contact info to footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () =>{
+    return(
+        <footer className=" w-full">
+            <p><strong>Contact Us:</strong> Mtendere Kulemeka(MK): mkulemeka@temple.edu <strong>||</strong> Joe Michel: joseph.michel0001@temple.edu</p>
+        </footer>
+    );
+};
+
+export default Footer;

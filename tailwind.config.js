68305a09695c01e9a15bac251b3fa1d619e7b30e
mkulemeka <kahlekulemeka@gmail.com>
2023-09-24 09:02:40 -0400

    styling for navigation done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-import {nextui} from "@nextui-org/react";
+import { nextui } from "@nextui-org/react";
 
 export default {
   content: [
@@ -10,7 +10,13 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        OleoScript: ["Oleo Script", "cursive"],
+        Valera: ["Varela Round", "sans-serif"],
+      },
+      fontSize: { heading: "4rem", subheading: "2rem", body: "1rem" },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
